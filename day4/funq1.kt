e96ae79137a90315aa11c84903d8b3cf5b469091
Pradyumna-inf <[Pradyumnakumarbehera10@gmail.com]>
2024-06-29 16:01:01 +0530

    added kotlin file

---
+++ day4/funq1.kt
@@ -0,0 +1,14 @@
+fun main() {
+    
+    larger(2,3)
+}
+fun larger(n1:Int,n2:Int){
+    if(n1>n2){
+        println("${n1} is larger")
+    }
+    else{
+        println("${n2} is larger")
+    }
+}
+
+
